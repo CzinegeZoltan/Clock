bb0b0cd724436145a4d21e9e591b9185dbd3b6a0
CzinegeZoltan <144321567+CzinegeZoltan@users.noreply.github.com>
2024-01-05 15:12:20 +0100

    Add files via upload

---
+++ Clock/Clock/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Black;
+            this.lblTime.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(27, 23);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 15);
+            this.lblTime.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(244, 101);
+            this.Controls.Add(this.lblTime);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(260, 140);
+            this.Name = "Form1";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ Clock/Clock/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        private const float DefaultFontSize = 36;
+        private const int MinimumFormWidth = 382;
+        private const int MinimumFormHeight = 289;
+        private Timer timer;
+        private bool colonVisible;
+
+        public Form1()
+        {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.Resize += Form1_Resize;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            lblTime.AutoSize = false;
+            lblTime.Dock = DockStyle.Fill;
+            lblTime.TextAlign = ContentAlignment.MiddleCenter;
+
+            SetInitialFontSize();
+            SetDoubleBuffered(this);
+            SetDoubleBuffered(lblTime);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            colonVisible = DateTime.Now.Second % 2 == 0;
+            lblTime.Text = GetFormattedTime();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            SetFontSize();
+        }
+
+        private void SetInitialFontSize()
+        {
+            float fontSize = Math.Max(DefaultFontSize, Math.Min(this.Width, this.Height) / 3.9f);
+            lblTime.Font = new Font(lblTime.Font.FontFamily, fontSize, FontStyle.Bold);
+        }
+
+        private void SetFontSize()
+        {
+            float fontSize = Math.Max(DefaultFontSize, Math.Min(this.Width, this.Height) / 3.9f);
+            lblTime.Font = new Font(lblTime.Font.FontFamily, fontSize, FontStyle.Bold);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        private void SetDoubleBuffered(Control control)
+        {
+            control.GetType().GetProperty("DoubleBuffered", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)?.SetValue(control, true, null);
+        }
+
+        private string GetFormattedTime()
+        {
+            string timeText = DateTime.Now.ToString("HH:mm:ss\nyyyy.MM.dd");
+            if (!colonVisible)
+            {
+                timeText = timeText.Replace(":", " ");
+            }
+            return timeText;
+        }
+    }
+}
